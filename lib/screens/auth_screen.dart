1aa6f1c44a71eee5b886e6ae8d839fe6e619bae2
IslamibmGH <mac@MACs-MacBook-Air.local>
2021-09-21 10:31:50 +0200

    My first commit

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,137 @@
+import 'package:chat_application/main.dart';
+import 'package:chat_application/screens/home_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:chat_application/constants.dart';
+import '../components/primary_button.dart';
+
+class AuthScreen extends StatefulWidget {
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  GoogleSignIn googleSignIn = GoogleSignIn();
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future signInFunction() async {
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    if (googleUser == null) {
+      return;
+    }
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+    UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+    DocumentSnapshot userExist =
+        await firestore.collection('users').doc(userCredential.user!.uid).get();
+
+    if (userExist.exists) {
+      print("User Already Exists in Database");
+    } else {
+      await firestore.collection('users').doc(userCredential.user!.uid).set({
+        'email': userCredential.user!.email,
+        'name': userCredential.user!.displayName,
+        'image': userCredential.user!.photoURL,
+        'uid': userCredential.user!.uid,
+        'date': DateTime.now(),
+      });
+    }
+
+    Navigator.pushAndRemoveUntil(context,
+        MaterialPageRoute(builder: (context) => MyApp()), (route) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           children: [
+//             Expanded(
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                     image: DecorationImage(
+//                         image: NetworkImage(
+//                             "https://cdn.iconscout.com/icon/free/png-256/chat-2639493-2187526.png"))),
+//               ),
+//             ),
+//             Text(
+//               "",
+//               style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+//               child: ElevatedButton(
+//                 onPressed: () async {
+//                   await signInFunction();
+//                 },
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Image.network(
+//                       'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png',
+//                       height: 36,
+//                     ),
+//                     SizedBox(
+//                       width: 10,
+//                     ),
+//                     Text(
+//                       "Sign in With Google",
+//                       style: TextStyle(fontSize: 20),
+//                     )
+//                   ],
+//                 ),
+//                 style: ButtonStyle(
+//                     backgroundColor: MaterialStateProperty.all(Colors.black),
+//                     padding: MaterialStateProperty.all(
+//                         EdgeInsets.symmetric(vertical: 12))),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: kDefaultPadding, vertical: 50),
+          child: Column(
+            children: [
+              Spacer(flex: 2),
+              Image.asset(
+                MediaQuery.of(context).platformBrightness == Brightness.light
+                    ? "assets/images/Logo_light.png"
+                    : "assets/images/Logo_dark.png",
+                height: 146,
+              ),
+              Spacer(),
+              PrimaryButton(
+                text: "Sign In",
+                press: () async {
+                  await signInFunction();
+                  {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MyApp(),
+                      ),
+                    );
+                  }
+                },
+              ),
+              SizedBox(height: 250),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
